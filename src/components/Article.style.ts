3faad15994195fd5f04caf9e3398fd6bcd964bc4
fernando predes <fernando_predes@hotmail.com>
2022-11-17 18:54:12 -0300

    cards faltando colocar fontes

--- src/components/Article.style.ts
+++ src/components/Article.style.ts
@@ -1,17 +1,86 @@
 import styled from 'styled-components'
 import art1 from "../assets/artigo1.png"
-import art2 from "../assets/artigo1.png"
+import art2 from "../assets/artigo2.png"
 
 export const ArticleCard = styled.article`
 
-  max-height: 395px;
- .bg{
+
+
+
+  h3{
+  margin-top:129px;
+  margin-left: 1.2rem;
+  font-family: 'Inter';
+  font-style: normal;
+  font-weight: 600;
+  font-size: 32px;
+  line-height: 38px;
+  color: #F5F5F5;
+  }
+
+  h3 span{
+    font-size: 50px;
+    color: #FFFFFF;
+    opacity: 0.5;
+    letter-spacing: -3px;
+
+  }
+
+  h4{
+    font-family: 'Roboto';
+    margin-left: 1.2rem;
+    font-style: normal;
+    font-weight: 500;
+    font-size: 22.0556px;
+    line-height: 150%;
+    letter-spacing: -0.011em;
+    color: #F5F5F5;
+  }
+
+ .bg1{
     background: url(${art1}) no-repeat center center ;
     -webkit-background-size: cover;
     -moz-background-size: cover;
     -o-background-size: cover;
-    background-size: cover;
+    background-size: auto ;
+    -webkit-border-radius: 50px;
+    -moz-border-radius: 50px;
+    border-radius: 15px;
+    border: none;
+
 }
 
+.bg2{
+    background: url(${art2}) no-repeat center top ;
+    -webkit-background-size: cover;
+    -moz-background-size: cover;
+    -o-background-size: cover;
+    background-size: auto ;
+    -webkit-border-radius: 50px;
+    -moz-border-radius: 50px;
+    border-radius: 15px;
+    border: none;
+
+}
+
+.card{
+    max-width: 550px;
+    padding: 0;
+ }
+
+.readlink{
+  text-align: center;
+  background: #1FC4CC;
+  p{
+    font-family: 'Inter';
+    font-style: normal;
+    font-weight: 600;
+    font-size: 36.85px;
+    line-height: 45px;
+    color: #F5F5F5;
+    margin-bottom: 0;
+    padding-bottom: 14px;
+  }
+}
 
 `
--- src/components/Article.tsx
+++ src/components/Article.tsx
@@ -5,32 +5,32 @@ type ArticleProps = {}
 function Article({}: ArticleProps) {
 
   return (
-        <ArticleCard className='row justify-content-between'>
-          <div className="bg col-md-6">
+        <ArticleCard className='row justify-content-between g-5'>
+          <div className="bg1 col-lg-6 card">
             <div>
               <div>
-                <h3>documento online abre novas possiblidades para área da saúde <span><br/>______</span></h3>
+                <h3>documento online abre novas possiblidades para área da saúde <span><br/>----------</span></h3>
               </div>
             <div>
                 <h4>com o avanço da tencologia na área da saúde, redes públicas e particulares estão aderindo ao novo...</h4>
             </div>
             </div>
-              <div>
-              <p>ler mais</p>
+              <div className='readlink'>
+                <p>ler mais</p>
               </div>
             </div>
 
-            <div className="bg col-md-6">
+            <div className="bg2 col-lg-6 card">
             <div>
               <div>
-                <h3>documento online abre novas possiblidades para área da saúde <span><br/>______</span></h3>
+                <h3>documento online abre novas possiblidades para área da saúde <span><br/>----------</span></h3>
               </div>
             <div>
                 <h4>com o avanço da tencologia na área da saúde, redes públicas e particulares estão aderindo ao novo...</h4>
             </div>
             </div>
-              <div>
-              <p>ler mais</p>
+              <div className='readlink'>
+                <p>ler mais</p>
               </div>
             </div>
         </ArticleCard>
