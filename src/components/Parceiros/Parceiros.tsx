3b17738c48928369a95e86803e5493b91ecdba41
fernando predes <fernando_predes@hotmail.com>
2022-11-20 14:14:35 -0300

    componente responsivo

--- src/components/Parceiros/Parceiros.tsx
+++ src/components/Parceiros/Parceiros.tsx
@@ -7,9 +7,9 @@ import { ParceirosStyle } from "./HighLight.style"
 const Parceiros = () => {
   return (
     <ParceirosStyle className="row d-flex justify-content-center ">
-      <div className="col-2 text-center"><img src={unimed} alt="logo Unimed" /></div>
-      <div className="col-2 text-center"><img src={sabin} alt="logo Sabin" /></div>
-      <div className="col-2 text-center"><img src={conect} alt="logo Conect Sus" /></div>
+      <div className="col-lg-2 col-md-12 col-sm-12 col-12 text-center unimed"><img src={unimed} alt="logo Unimed" /></div>
+      <div className="col-lg-2 col-md-6 col-sm-6 col-6 text-center"><img src={sabin} alt="logo Sabin" /></div>
+      <div className="col-lg-2 col-md-6 col-sm-6 col-6 text-center"><img src={conect} alt="logo Conect Sus" /></div>
     </ParceirosStyle>
   )
 }
