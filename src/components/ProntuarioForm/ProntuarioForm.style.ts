a8b421f413e31b92c28f13a95a85a3e769c56d43
fernando predes <fernando_predes@hotmail.com>
2022-11-20 20:46:30 -0300

    textos ok

--- src/components/ProntuarioForm/ProntuarioForm.style.ts
+++ src/components/ProntuarioForm/ProntuarioForm.style.ts
@@ -1,12 +1,38 @@
 import styled from "styled-components";
 
 export const  ProntuarioStyle = styled.form`
+
+background: #F5F5F5;
+border: 0.9875px solid #CFCFCF;
+border-radius: 15.8px;
+padding: 33px 30px;
+font-family: 'Roboto';
+font-style: normal;
+font-weight: 400;
+font-size: 20px;
+line-height: 23px;
+
+input{
+  border-radius: 10px;
+  border: #DDDFE1 solid 1px;
+}
+input::placeholder{
+  background-color: #FAFAFA;
+  color: #abafb3;
+  font-size: 14px;
+
+}
+
   .obrigatorio{
     display: contents;
     color: #DC2113;
   }
   .nome{
     display: grid;
+    input{
+      max-width: 967px;
+      min-height: 53px;
+    }
   }
 
   .email{
