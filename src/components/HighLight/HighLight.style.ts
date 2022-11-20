37c0ada33b8c26bff3bbaa296a88403f17e72cd8
fernando predes <fernando_predes@hotmail.com>
2022-11-20 12:14:30 -0300

    responsividade ok

--- src/components/HighLight/HighLight.style.ts
+++ src/components/HighLight/HighLight.style.ts
@@ -9,8 +9,23 @@ export const  HighLight = styled.h2`
   font-style: normal;
   font-weight: 600;
   font-size: 52px;
-  line-height: 61px;
   color: #000000;
+  @media only screen and (max-width: 767px){
+    font-size: 40px;
+
+   }
+   @media only screen and (max-width: 454px){
+    font-size: 35px;
+
+   }
+  @media only screen and (max-width: 402px){
+    font-size: 31px;
+
+   }
+   @media only screen and (max-width: 360px){
+    font-size: 24px;
+
+   }
 
 `
 export const  DivHighLight = styled.div`
