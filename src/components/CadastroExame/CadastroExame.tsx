4e26c48558c31519f3426a439839bf700facd100
Patrick Segalla <patricksegalla22@gmail.com>
2022-11-24 01:50:14 -0300

    Componente de cadastro de exame finalizado

---
+++ src/components/CadastroExame/CadastroExame.tsx
@@ -0,0 +1,65 @@
+import { CadastroExameStyle, AceiteTermos } from "./CadastroExame.style";
+
+
+
+const CadastroExame = () => {
+  return (
+    <>
+        <CadastroExameStyle>
+            <div className='row'>
+            <div className='col-12 col-md-12 '>
+                <label className='exame'>
+                Exame
+                <input placeholder="Digite aqui o nome do seu exame" />
+                </label>
+            </div>
+            </div>
+            <div className='row'>
+            <div className='col-12 col-md-12 '>
+                <label className='data'>
+                Data
+                <input placeholder="__/__/__" />
+                </label>
+            </div>
+            </div>
+            <div className='row'>
+            <div className='col-12 col-md-12 '>
+                <label className='diag'>
+                Diagnóstico
+                <input placeholder="Digite aqui seu diagnóstico" />
+                </label>
+            </div>
+            </div>
+            <div className='row'>
+            <div className='col-12 col-md-12 '>
+                <label className='local'>
+                Local
+                <input placeholder="Local em que o exame foi feito" />
+                </label>
+            </div>
+            </div>
+            <div className='row'>
+            <div className='col-12 col-md-12 '>
+                <label className='medico'>
+                Médico Responsável
+                <input placeholder="Nome do médico" />
+                </label>
+            </div>
+            </div>
+            <div className='row'>
+            <div className='col-12 col-md-12 '>
+                <label className='obs'>
+                Observação
+                <textarea placeholder="Digite aqui as observações do seu exame" />
+                </label>
+            </div>
+            </div>
+        </CadastroExameStyle>
+        <AceiteTermos>
+            <p className="termo"><input type="checkbox"  checked />Aceito e estou de acordo com o termos de uso deste site, bem como o armazenamento dos meus dados pessoais confiando na totla segurança e privacidade.</p>
+        </AceiteTermos>
+    </>
+  )
+}
+
+export default CadastroExame
--- src/pages/Exames.tsx
+++ src/pages/Exames.tsx
@@ -1,5 +1,6 @@
 import BemVindo from "../components/BemVindo/BemVindo"
 import Button from "../components/Button/Button"
+import CadastroExame from "../components/CadastroExame/CadastroExame"
 import { TopSection } from "./Exames.style"
 
 
@@ -7,18 +8,20 @@ type ExamesProps = {}
 
 const Exames = (props: ExamesProps) => {
   return (
-    <TopSection className="container d-flex mobile">
-      <div>
-        <BemVindo
-          user={"Fernando Predes"}
-          text={"Insira aqui os dados do seu exame"}
-          subtext={"*Insira e edite os dados sobre o exame que foi realizado"}
-          />
-      </div>
-      <div className="d-flex align-items-end pb-5">
-        <Button text="teste"/>
-      </div>
-    </TopSection>
+   <>
+      <TopSection className="container d-flex mobile">
+        <div>
+          <BemVindo
+            user={"Fernando Predes"}
+            text={"Insira aqui os dados do seu exame"}
+            subtext={"*Insira e edite os dados sobre o exame que foi realizado"}
+            />
+        </div>
+        <div className="d-flex align-items-end pb-5">
+          <Button text="teste"/>
+        </div>
+      </TopSection>
+    </>
   )
 }
 
