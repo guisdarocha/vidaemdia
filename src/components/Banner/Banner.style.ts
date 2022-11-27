5dcbc26f33a97254937622ec50d9eea2090910ce
Wesley Militão <wesmilitao@gmail.com>
2022-11-27 14:13:14 -0300

    responsividade dos modais

--- src/components/Banner/Banner.style.ts
+++ src/components/Banner/Banner.style.ts
@@ -1,9 +1,14 @@
 import styled from 'styled-components';
 
-export const carousel = styled.div`
+
+export const Carousel = styled.div`
     img {
         width: 90%;
         height: 389px;
         margin: 148px 65px 0 48px;
+    }
+
+    @media only screen and (max-width: 428px) {
+     
     }
 `;
