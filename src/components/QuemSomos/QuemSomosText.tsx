7ad0f06f6769a36b41726aa904d34fa2b8b7ab2b
fernando predes <fernando_predes@hotmail.com>
2022-11-28 00:29:52 -0300

    tirado texto duplicado

--- src/components/QuemSomos/QuemSomosText.tsx
+++ src/components/QuemSomos/QuemSomosText.tsx
@@ -10,7 +10,7 @@ const QuemSomosText = () => {
                 <HighLight className="HighLight">Quem somos</HighLight>
                 <h2 className="text1">Somos uma startup em expansão na área da saúde como foco na humanização e agilidades na formalização de prontuários digitais de pacientes objetivando a redução de espera no atendimento clínico em hospitais, clínicas e/ou laboratórios otimizando assim o tempo no recebimento, armazenamento e leitura de dados do paciente em seu prontuário online.
                 </h2>
-                <h2 className="text2">Somos uma startup em expansão na área da saúde como foco na humanização e agilidades na formalização de prontuários digitais de pacientes objetivando a redução de espera no atendimento clínico em hospitais, clínicas e/ou laboratórios otimizando assim o tempo no recebimento, armazenamento e leitura de dados do paciente em seu prontuário online.
+                <h2 className="text2">É claro que as startups se caracterizam como empresas que entraram há pouco no mercado. Por isso, nos comprometemos a criar soluções e inovações que realmente impactem no seu ecossistema. Só que, nesse caso, o objetivo é apoiar a rede médica e os pacientes nas suas atividades.
                 </h2>
                 <h2 className="desfoque">“Com os avanços tecnológicos pelos quais estamos constantemente passando, praticamente todos os setores do mercado se viram obrigados a adotar algumas adaptações — incluindo o da saúde. Uma das novidades que foi introduzida ao segmento foi o prontuário online, que surgiu para trazer mais facilidade e aumentar a segurança.”
                 </h2>
